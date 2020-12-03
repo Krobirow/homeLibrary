be99024ae6026925808cb0efb05251e6ee5cb654
Krobirow <bodia122@gmail.com>
2020-12-03 18:33:02 +0100

    style and validation fixes

--- src/utils/validators/validators.js
+++ src/utils/validators/validators.js
@@ -2,7 +2,7 @@
 export const required = value => value ? undefined : 'Field is required';
 
 export const numberOnly = value => value && /[^0-9]/g.test(value) ? 'Numbers only required' : undefined;
-export const name = value => value && /[^A-Za-z]/g.test(value) ? 'Letters only required' : undefined;
+export const name = value => value && /[^A-Za-z ]/g.test(value) ? 'Letters only required' : undefined;
 
 export const maxlengthCreator = maxLength => value => value && value.length > maxLength ? `Max length is ${maxLength} characters` : undefined;
 
