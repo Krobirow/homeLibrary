204c08ef6ccc80c8216d7363659f531a3d41df6f
Krobirow <bodia122@gmail.com>
2020-12-03 17:44:36 +0100

    style fixes

--- src/components/books/bookCard/createNewBook/createNewBook.js
+++ src/components/books/bookCard/createNewBook/createNewBook.js
@@ -15,9 +15,11 @@ const  NewBookForm = (props) =>  {
             
             <form onSubmit={props.handleSubmit}>
                 <h5>Add Some Book</h5>
+                <div>
                 {createField("text", "Provie an author name", "authorName", [required, maxStringLength], Input, "col-12 form-control authorName")}
                 {createField("text", "Provie a book title", "bookTitle", [required, maxStringLength], Input, "col-12 form-control booktitle")}
                 {createField("text", "Provie a publication date", "publicationDate", [required, maxNumLength, numbers], Input, "col-12 form-control publicationDate")}
+                </div>
                 <button type="submit" className={"btn btn-primary mt-2"} >Add Book</button>
             </form>
         );
@@ -27,7 +29,7 @@ const ReduxNewBookForm =  reduxForm({form:"formCreateNewBook"})(NewBookForm);
 
 const CreateNewBook = (props) => {
     return (
-        <ZoomInUpDiv className="card col-xl-4 col-lg-4 col-md-5 col-sm-5 col-12 no-gutters p-3">
+        <ZoomInUpDiv className="card col-xl-4 col-lg-4 col-md-5 offset-sm-1 col-sm-10 col-12 no-gutters p-3">
             <ReduxNewBookForm onSubmit={props.onSubmit} />
         </ZoomInUpDiv>
     );
